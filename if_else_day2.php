f21936ab815a923be1a8208cbe824f48f96e678a
Mehul-dev27 <72722248+Mehul-dev27@users.noreply.github.com>
2021-05-28 21:06:41 +0530

    Day 2

---
+++ if_else_day2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $a = 25;
+    $b = 50;
+
+    if($a<$b){
+        echo "a is smaller than b";
+    }else if($a == $b){
+        echo "a and b are equal";
+    }else {
+        echo "a is greater than b"
+    }
+
+?>
